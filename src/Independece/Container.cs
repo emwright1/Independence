13a3d7f5d4da541f4c78f517df609b36606dfd00
Sean Adamson <git@seanadamson.com>
2015-09-26 15:27:00 +0100

    added files

---
+++ src/Independece/Container.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Independece
+{
+    public class Container
+    {
+        private Dictionary<Type, object> RegisteredServices = new Dictionary<Type, object>();
+
+        public void Register<TType, TImp>(TImp Impementation) where TImp : TType
+        {
+            RegisteredServices.Add(typeof(TType), Impementation);
+        }
+
+        public void Register<TType, TImp>(Func<TImp> factory) where TImp : TType
+        {
+            RegisteredServices.Add(typeof(TType), factory());
+        }
+
+        public TImp Resolve<TType, TImp>(TType type)
+        {
+            return (TImp)RegisteredServices[typeof(TType)];
+        }
+
+        public T Resolve<T>() where T : class
+        {
+            return Activator.CreateInstance<T>();
+        }
+
+        public T Resolve<T, TImp>() where T : class
+        {
+            var constructors = typeof(T).GetConstructors();
+
+            foreach (var constructor in constructors)
+            {
+                // Put them into a collection ordered by number of params.
+            }
+
+            // resolve the constructor with the most amount of params you can resolve.
+
+            return (T)null;
+        }
+    }
+}
---
+++ tests/Independence.Tests/ContainerTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Independece;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestAttributes;
+
+namespace Independence.Tests
+{
+    public class ContainerTests
+    {
+        public class Test : ITest { }
+        public interface ITest { }
+
+        internal class Resolve
+        {
+            [Unit]
+            public void WhenServiceIsRegisteredReturnInstance()
+            {
+                var container = new Container();
+                var resolution = container.Resolve<Test>();
+                resolution.Should().NotBeNull();
+            }
+
+            [Unit]
+            public void GivenFactoryActionReturnInstance()
+            {
+
+            }
+        }
+    }
+}
